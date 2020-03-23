84309f43d9b82d365d43f4717ef9533b16322ba6
sreenithy c <sreenithyc21@gmail.com>
2020-03-22 21:03:02 -0700

    Updates light transport acquisition-sl projection generation

--- EpiscanScriptDemo/scripts/calibration.cs
+++ EpiscanScriptDemo/scripts/calibration.cs
@@ -10,8 +10,9 @@ using System.IO;
 public class calibration : IEpiscanScript
 {
     public string PatternDir1 { set; get; } = "pos";
-    public string OutputDir { set; get; } = "capture";
-    public int AverageNum { set; get; } = 5;
+    public string PatternDir2 { set; get; } = "neg";
+    public string OutputDir { set; get; } = "newcapture";
+
 
     public void Configure(MyEpiscan episcan)
     {
@@ -21,22 +22,25 @@ public class calibration : IEpiscanScript
     public async Task Run(MyEpiscan episcan)
     {
         float delay = 1f;
-        float exposure = 0.05f;
+        float exposure = 0.10f;
         int pc2 = 60;
         int gain = 0;
-        
+        int AverageNum = 5;
         episcan.Sensor.Delay = (int)(delay - 0.5f * exposure * 1e3f);
         episcan.Sensor.Exposure = exposure;
         episcan.Sensor.PixelClock = pc2;
         episcan.Sensor.MasterGain = gain;
-       
         episcan.Sensor.ShutterMode = MySensor.ShutterModeList.Rolling;
+
         var patternFiles = Directory.GetFiles(PatternDir1, "*.png").ToArray();
         var patterns = patternFiles.Select(src => Cv2.ImRead(src, ImreadModes.Unchanged)).ToArray();
 
-        while (delay <100f)
-        {   
-            if (!Directory.Exists(OutputDir)) Directory.CreateDirectory(OutputDir);
+
+        while (delay < 10000f)
+        {
+            Console.WriteLine(delay);
+            string OutputDir1 = OutputDir + "\\pos_" + delay.ToString();
+            if (!Directory.Exists(OutputDir1)) Directory.CreateDirectory(OutputDir1);
 
             for (int i = 0; i < patterns.Count(); i++)
             {
@@ -44,11 +48,40 @@ public class calibration : IEpiscanScript
                 await Task.Delay(100);
                 var capture = await episcan.CaptureAverage(AverageNum);
                 var filename = Path.GetFileName(patternFiles[i]);
-                Cv2.ImWrite($"{OutputDir}\\{filename}", capture);
+                Cv2.ImWrite($"{OutputDir1}\\{filename}", capture);
             }
-            delay = delay + 10f;
-
+            delay = delay + 100f;
         }
-        
+
+        //Negative Patterns
+        delay = 1f;
+
+        episcan.Sensor.Delay = (int)(delay - 0.5f * exposure * 1e3f);
+        episcan.Sensor.Exposure = exposure;
+        episcan.Sensor.PixelClock = pc2;
+        episcan.Sensor.MasterGain = gain;
+        episcan.Sensor.ShutterMode = MySensor.ShutterModeList.Rolling;
+
+        var patternFilesNeg = Directory.GetFiles(PatternDir2, "*.png").ToArray();
+        var patternsNeg = patternFilesNeg.Select(src => Cv2.ImRead(src, ImreadModes.Unchanged)).ToArray();
+
+
+        while (delay < 10000f)
+        {
+            Console.WriteLine(delay);
+            string OutputDir1 = OutputDir + "\\neg_" + delay.ToString();
+            if (!Directory.Exists(OutputDir1)) Directory.CreateDirectory(OutputDir1);
+
+            for (int i = 0; i < patternsNeg.Count(); i++)
+            {
+                episcan.Screen.BackgroundMat = patternsNeg[i];
+                await Task.Delay(100);
+                var capture = await episcan.CaptureAverage(AverageNum);
+                var filename = Path.GetFileName(patternFilesNeg[i]);
+                Cv2.ImWrite($"{OutputDir1}\\{filename}", capture);
+            }
+            delay = delay + 100f;
+        }
+
     }
 }
--- scripts/calibration.cs
+++ scripts/calibration.cs
@@ -9,9 +9,10 @@ using System.IO;
 
 public class calibration : IEpiscanScript
 {
-    public string PatternDir { set; get; } = "patterns";
-    public string OutputDir { set; get; } = "capture";
-    public int AverageNum { set; get; } = 5;
+    public string PatternDir1 { set; get; } = "pos";
+    public string PatternDir2 { set; get; } = "neg";
+    public string OutputDir { set; get; } = "newcapture";
+
 
     public void Configure(MyEpiscan episcan)
     {
@@ -20,18 +21,67 @@ public class calibration : IEpiscanScript
 
     public async Task Run(MyEpiscan episcan)
     {
-        var patternFiles = Directory.GetFiles(PatternDir, "*.png").ToArray();
+        float delay = 1f;
+        float exposure = 0.10f;
+        int pc2 = 60;
+        int gain = 0;
+        int AverageNum = 5;
+        episcan.Sensor.Delay = (int)(delay - 0.5f * exposure * 1e3f);
+        episcan.Sensor.Exposure = exposure;
+        episcan.Sensor.PixelClock = pc2;
+        episcan.Sensor.MasterGain = gain;
+        episcan.Sensor.ShutterMode = MySensor.ShutterModeList.Rolling;
+
+        var patternFiles = Directory.GetFiles(PatternDir1, "*.png").ToArray();
         var patterns = patternFiles.Select(src => Cv2.ImRead(src, ImreadModes.Unchanged)).ToArray();
 
-        if (!Directory.Exists(OutputDir)) Directory.CreateDirectory(OutputDir);
 
-        for (int i = 0; i < patterns.Count(); i++)
+        while (delay < 10000f)
         {
-            episcan.Screen.BackgroundMat = patterns[i];
-            await Task.Delay(100);
-            var capture = await episcan.CaptureAverage(AverageNum);
-            var filename = Path.GetFileName(patternFiles[i]);
-            Cv2.ImWrite($"{OutputDir}\\{filename}", capture);
+            Console.WriteLine(delay);
+            string OutputDir1 = OutputDir + "\\pos_" + delay.ToString();
+            if (!Directory.Exists(OutputDir1)) Directory.CreateDirectory(OutputDir1);
+
+            for (int i = 0; i < patterns.Count(); i++)
+            {
+                episcan.Screen.BackgroundMat = patterns[i];
+                await Task.Delay(100);
+                var capture = await episcan.CaptureAverage(AverageNum);
+                var filename = Path.GetFileName(patternFiles[i]);
+                Cv2.ImWrite($"{OutputDir1}\\{filename}", capture);
+            }
+            delay = delay + 100f;
         }
+
+        //Negative Patterns
+        delay = 1f;
+
+        episcan.Sensor.Delay = (int)(delay - 0.5f * exposure * 1e3f);
+        episcan.Sensor.Exposure = exposure;
+        episcan.Sensor.PixelClock = pc2;
+        episcan.Sensor.MasterGain = gain;
+        episcan.Sensor.ShutterMode = MySensor.ShutterModeList.Rolling;
+
+        var patternFilesNeg = Directory.GetFiles(PatternDir2, "*.png").ToArray();
+        var patternsNeg = patternFilesNeg.Select(src => Cv2.ImRead(src, ImreadModes.Unchanged)).ToArray();
+
+
+        while (delay < 10000f)
+        {
+            Console.WriteLine(delay);
+            string OutputDir1 = OutputDir + "\\neg_" + delay.ToString();
+            if (!Directory.Exists(OutputDir1)) Directory.CreateDirectory(OutputDir1);
+
+            for (int i = 0; i < patternsNeg.Count(); i++)
+            {
+                episcan.Screen.BackgroundMat = patternsNeg[i];
+                await Task.Delay(100);
+                var capture = await episcan.CaptureAverage(AverageNum);
+                var filename = Path.GetFileName(patternFilesNeg[i]);
+                Cv2.ImWrite($"{OutputDir1}\\{filename}", capture);
+            }
+            delay = delay + 100f;
+        }
+
     }
 }
